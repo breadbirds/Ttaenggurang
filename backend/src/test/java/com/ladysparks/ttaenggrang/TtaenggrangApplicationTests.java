79f8aa182b3c08999f4880a4dd91c17834702dc5
최연지 <rokmc3038@naver.com>
2025-01-23 17:20:05 +0900

    프로젝트 생성

---
+++ backend/src/test/java/com/ladysparks/ttaenggrang/TtaenggrangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ladysparks.ttaenggrang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtaenggrangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
