8a83cad685fdbb8a98fedfc438c5989039205b24
서미지 <itsanisland@naver.com>
2025-01-24 13:02:54 +0900

    feat: Item 전체 조회 구현
    S12P11D107-180 #in-progress

--- backend/src/main/java/com/ladysparks/ttaenggrang/docs/ItemApiSpecification.java
+++ backend/src/main/java/com/ladysparks/ttaenggrang/docs/ItemApiSpecification.java
@@ -8,14 +8,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
+
+@Tag(name = "Item", description = "아이템 거래 관련 API")
 public interface ItemApiSpecification {
 
-    @Tag(name = "Item", description = "아이템 거래 관련 API")
-    @Operation(summary = "아이템 조회", description = "💡 아이템 ID로 아이템을 조회합니다.")
+    @Operation(summary = "아이템 전체 조회", description = "💡 전체 아이템을 조회합니다.")
+    @GetMapping
+    public ResponseEntity<List<Item>> getItems();
+
+    @Operation(summary = "아이템 상세 조회", description = "💡 아이템 ID로 아이템을 조회합니다.")
     @GetMapping
     public ResponseEntity<Item> getItem(@PathVariable("itemId") int itemId);
 
-    @Tag(name = "Item", description = "아이템 거래 관련 API")
     @Operation(summary = "아이템 등록", description = "💡 판매할 아이템을 등록합니다.")
     @GetMapping
     public ResponseEntity<Item> postItem(@RequestBody Item item);
