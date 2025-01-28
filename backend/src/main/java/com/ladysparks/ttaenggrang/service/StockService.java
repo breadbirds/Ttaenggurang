c7eec10d0c40edb9e968cf10181d3aa7a54e6318
최연지 <rokmc3038@naver.com>
2025-01-28 23:42:50 +0900

    feat: 주식 목록 조회
    - 주식 목록 조회(학생)
    
    S12P11D107-199 #done 작업완료

--- backend/src/main/java/com/ladysparks/ttaenggrang/service/StockService.java
+++ backend/src/main/java/com/ladysparks/ttaenggrang/service/StockService.java
@@ -1,4 +1,42 @@
 package com.ladysparks.ttaenggrang.service;
 
+import com.ladysparks.ttaenggrang.domain.stock.Stock;
+import com.ladysparks.ttaenggrang.dto.stock.StockDTO;
+import com.ladysparks.ttaenggrang.repository.StockRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class StockService {
+
+    private final StockRepository stockRepository; //의존성 주입
+
+    //목록 조회
+    public int saveStock(StockDTO stockDto) {
+        // StockDTO를 Stock 엔티티로 변환
+        Stock stock = StockDTO.toEntity(stockDto);
+        // 변환된 엔티티를 DB에 저장
+        stockRepository.save(stock);
+        // 저장된 엔티티의 ID 반환
+        return stock.getId();
+    }
+
+    public List<StockDTO> findStocks() {
+        //모든 주식 데이터 조회
+        List<Stock> stocks = stockRepository.findAll();
+        // 조회된 Stock 엔티티 리스트를 StockDTO 리스트로 변환
+        return stocks.stream()
+                .map(StockDTO::fromEntity) // 엔티티를 DTO로 변환
+                .collect(Collectors.toList()); // 변환된 DTO를 리스트로 반환
+    }
+    public Optional<StockDTO> findStock(int stockId) {
+        // ID로 주식 조회 후, StockDTO로 변환하여 반환
+        return stockRepository.findById(stockId)
+                .map(StockDTO::fromEntity); // 엔티티를 DTO로 변환
+    }
 }
