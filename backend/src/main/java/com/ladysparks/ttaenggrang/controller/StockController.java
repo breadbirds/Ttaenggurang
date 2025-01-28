65402dfa2b155c1f1a0d2bf8173e4cd79c7e96a3
최연지 <rokmc3038@naver.com>
2025-01-28 23:55:11 +0900

    feat: 주식 상세  목록 조회
    - 주식 상세 목록 조회(학생)
    
    S12P11D107-200 #done 작업완료

--- backend/src/main/java/com/ladysparks/ttaenggrang/controller/StockController.java
+++ backend/src/main/java/com/ladysparks/ttaenggrang/controller/StockController.java
@@ -1,16 +1,16 @@
 package com.ladysparks.ttaenggrang.controller;
 
-import com.ladysparks.ttaenggrang.domain.stock.Stock;
-import com.ladysparks.ttaenggrang.dto.ItemDTO;
 import com.ladysparks.ttaenggrang.dto.stock.StockDTO;
 import com.ladysparks.ttaenggrang.service.StockService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,10 +18,23 @@ import java.util.List;
 public class StockController {
     private final StockService stockService; // StockService 주입
 
-    // 주식 목록 조회
+    // 주식 목록 전체 조회
     @GetMapping
     public ResponseEntity<List<StockDTO>> getStocks() {
         List<StockDTO> result = stockService.findStocks(); // 모든 주식 정보를 반환
         return ResponseEntity.ok(result); // HTTP 200 OK와 함께 결과 반환
+    }
+
+    // 주식 상세 조회
+    @GetMapping("/{stockId}")
+    public ResponseEntity<StockDTO> getStock(@PathVariable("stockId") int stockId) {
+        Optional<StockDTO> result = stockService.findStock(stockId);
+
+        // 값이 없으면 404 Not Found 반환
+        if (result.isPresent()) {
+            return ResponseEntity.ok(result.get()); // 값이 있으면 200 OK와 함께 결과 반환
+        } else {
+            return ResponseEntity.notFound().build(); // 값이 없으면 404 Not Found 반환
+        }
     }
 }
