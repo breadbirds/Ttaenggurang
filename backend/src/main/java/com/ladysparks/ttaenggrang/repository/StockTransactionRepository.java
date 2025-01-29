a4e7786db018c5ed9340cb266e26575c80ded1a5
최연지 <rokmc3038@naver.com>
2025-01-29 23:37:37 +0900

    feat: 주식거래내역§¤수
    - 주ì ì업중

--- backend/src/main/java/com/ladysparks/ttaenggrang/repository/StockTransactionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ladysparks.ttaenggrang.repository;
-
-import com.ladysparks.ttaenggrang.domain.stock.Stock;
-import com.ladysparks.ttaenggrang.domain.stock.StockTransaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StockTransactionRepository extends JpaRepository<StockTransaction, Integer> {
-}
