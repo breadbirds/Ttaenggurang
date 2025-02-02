87f24638e3ba306261e0a5bac861e4c412924ca7
jeongin <harperim127@gmail.com>
2025-02-03 05:28:27 +0900

    feat: 학ì생 계정 생계정 등록록 기능 구현
    - í - 학생계정 등록 기능 구현
    - 토큰 관리 보완 필요

--- backend/src/main/java/com/ladysparks/ttaenggrang/domain/user/entity/Teacher.java
+++ backend/src/main/java/com/ladysparks/ttaenggrang/domain/user/entity/Teacher.java
@@ -36,12 +36,15 @@ public class Teacher {
     @Column(nullable = false, updatable = false)
     private Timestamp createdAt;
 
-    // 조인
+    // <조인>
+    // 학생
+    @OneToMany
+    @JoinColumn(name = "studentId", nullable = false)
+    private List<Student> student;
 
     //주식
     @OneToMany(mappedBy = "teacher", fetch = FetchType.LAZY)
     private List<Stock> stocks; // 선생님이 관리하는 주식 목록
-
 
     //주식
     @OneToMany(mappedBy = "teacher", fetch = FetchType.LAZY)
