a1c208d5f2d20713be0e120a64005b96fac8032d
서미지 <itsanisland@naver.com>
2025-02-03 15:00:52 +0900

    merge: 브랜치 병합

--- backend/src/main/java/com/ladysparks/ttaenggrang/global/docs/TeacherApiSpecification.java
+++ backend/src/main/java/com/ladysparks/ttaenggrang/global/docs/TeacherApiSpecification.java
@@ -1,10 +1,12 @@
 package com.ladysparks.ttaenggrang.global.docs;
 
+import com.ladysparks.ttaenggrang.domain.user.dto.JobCreateDTO;
 import com.ladysparks.ttaenggrang.domain.user.dto.TeacherLoginDTO;
 import com.ladysparks.ttaenggrang.domain.user.dto.TeacherSignupDTO;
 import com.ladysparks.ttaenggrang.global.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,5 +23,9 @@ public interface TeacherApiSpecification {
     @Operation(summary = "로그인", description = "💡 교사의 로그인을 진행합니다.")
     @PostMapping("/login")
     ResponseEntity<ApiResponse<TeacherLoginDTO>> login(@RequestBody TeacherLoginDTO teacherLoginDTO);
+
+    @Operation(summary = "직업 [등록]", description = "💡 교사가 새로운 직업을 등록합니다.")
+    @PostMapping("/jobs/create")
+    ResponseEntity<ApiResponse<?>> createJob(@RequestBody @Valid JobCreateDTO jobCreateDTO);
 
 }
