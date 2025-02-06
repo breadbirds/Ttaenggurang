f9c66b0b66ec3c191656985ebcc6a88ebf1675e1
임정인 <harperim127@gmail.com>
2025-02-06 20:08:48 +0900

    feat: 로그아웃 기능 추가 (학생, 교사)
    - 학생 / 교사 로그아웃 기능 추가
    
    S12P11D107-251 #done 작업 완료

--- backend/src/main/java/com/ladysparks/ttaenggrang/global/docs/StudentAccountApiSpecification.java
+++ backend/src/main/java/com/ladysparks/ttaenggrang/global/docs/StudentAccountApiSpecification.java
@@ -4,6 +4,7 @@ import com.ladysparks.ttaenggrang.domain.user.dto.*;
 import com.ladysparks.ttaenggrang.global.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,10 @@ public interface StudentAccountApiSpecification {
     @Operation(summary = "학생 로그인", description = "💡 학생의 로그인을 진행합니다.")
     @PostMapping("/login")
     ResponseEntity<ApiResponse<StudentLoginResponseDTO>> loginStudents(@RequestBody @Valid StudentLoginRequestDTO studentLoginDTO);
+
+    @Operation(summary = "학생 로그아웃", description = "💡 학생 계정을 로그아웃합니다. JWT 토큰을 헤더에서 제거하세요.")
+    @PostMapping("/logout")
+    ResponseEntity<ApiResponse<String>> logoutStudent(HttpServletRequest request);
 
     @Operation(summary = "학생 목록 조회 (확인용)", description = "💡 가입한 학생의 목록을 조회합니다.")
     @PostMapping("/all")
