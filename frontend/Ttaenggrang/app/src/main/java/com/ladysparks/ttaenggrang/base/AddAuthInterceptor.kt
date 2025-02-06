243d2fdaeac27faed6aac4ef5da1c1acdb6dffdb
이사랑 <ipcp365@naver.com>
2025-02-06 09:46:05 +0900

    feat: 로그인 기능 수정
    - API 주소 /api 추가
    - 로그인시 Header Token 포함하지 않게 수정

--- frontend/Ttaenggrang/app/src/main/java/com/ladysparks/ttaenggrang/base/AddAuthInterceptor.kt
+++ frontend/Ttaenggrang/app/src/main/java/com/ladysparks/ttaenggrang/base/AddAuthInterceptor.kt
@@ -8,7 +8,15 @@ import java.io.IOException
 class AddAuthInterceptor : Interceptor {
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): Response {
-        val builder = chain.request().newBuilder()
+        val request = chain.request()
+
+//         로그인 요청일 경우 `Authorization` 헤더 추가하지 않음
+        if (request.url.encodedPath.contains("/teachers/login") ||
+            request.url.encodedPath.contains("/teachers/signup")) {
+            return chain.proceed(request)
+        }
+
+        val builder = request.newBuilder()
 
         val token = SharedPreferencesUtil.getValue(SharedPreferencesUtil.JWT_TOKEN_KEY, "")
         if (token.isNotEmpty()) {
