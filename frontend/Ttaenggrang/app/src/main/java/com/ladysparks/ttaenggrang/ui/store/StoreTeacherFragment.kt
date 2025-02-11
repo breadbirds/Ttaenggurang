ecd8e5ce75f6eef3592064f1825420621dfe2246
박진현 <pjiy1220@naver.com>
2025-02-11 16:08:05 +0900

    feat: 상품 수정 다이얼로그 재수정
    - 머지

--- frontend/Ttaenggrang/app/src/main/java/com/ladysparks/ttaenggrang/ui/store/StoreTeacherFragment.kt
+++ frontend/Ttaenggrang/app/src/main/java/com/ladysparks/ttaenggrang/ui/store/StoreTeacherFragment.kt
@@ -13,6 +13,10 @@ import com.ladysparks.ttaenggrang.data.remote.RetrofitUtil
 import com.ladysparks.ttaenggrang.databinding.FragmentStoreTeacherBinding
 import com.ladysparks.ttaenggrang.databinding.FragmentStudentsBinding
 import kotlinx.coroutines.launch
+import android.app.Dialog
+import androidx.appcompat.app.AlertDialog
+import com.ladysparks.ttaenggrang.databinding.DialogItemTeacherRegisterBinding
+
 
 class StoreTeacherFragment : BaseFragment<FragmentStoreTeacherBinding>(FragmentStoreTeacherBinding::bind, R.layout.fragment_store_teacher) {
 
@@ -20,7 +24,35 @@ class StoreTeacherFragment : BaseFragment<FragmentStoreTeacherBinding>(FragmentS
         super.onViewCreated(view, savedInstanceState)
 
         getTeacherItemList()
+        binding.apply {
+            binding.btnRegisterItem.setOnClickListener{
+                requestRegisterItem()
+            }
+        }
     }
+
+
+    private fun requestRegisterItem() {
+        val dialogBinding = DialogItemTeacherRegisterBinding.inflate(layoutInflater)
+        val dialog = AlertDialog.Builder(requireContext())
+            .setView(dialogBinding.root)
+            .create()
+
+        dialogBinding.btnClose.setOnClickListener {
+            dialog.dismiss()
+        }
+        dialogBinding.btnCancel.setOnClickListener {
+            dialog.dismiss()
+        }
+        dialogBinding.btnRegister.setOnClickListener {
+
+        }
+
+
+
+        dialog.show() // 다이얼로그 띄우기
+    }
+
 
     private fun getTeacherItemList() {
         lifecycleScope.launch {
@@ -29,6 +61,8 @@ class StoreTeacherFragment : BaseFragment<FragmentStoreTeacherBinding>(FragmentS
             }.onSuccess {
                 Log.d("Success", "${it}")
                 val registeredItems = it.data?: emptyList()
+                // 등록된 아이템 수
+                binding.textCountRegisteredItem.text = "${registeredItems.size}개"
                 if (registeredItems.isNotEmpty()) {
                     binding.textNullRegisteredItem.visibility = View.GONE
                     binding.recyclerRegistedItem.visibility = View.VISIBLE
