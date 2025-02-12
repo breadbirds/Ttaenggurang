de47c9ff38f2e57a0562259248960de9de599383
이사랑 <ipcp365@naver.com>
2025-02-12 14:30:24 +0900

    refactor: 프래그먼트 접근방법 변경

---
+++ frontend/Ttaenggrang/app/src/main/java/com/ladysparks/ttaenggrang/data/model/response/NationInfoResponse.kt
@@ -0,0 +1,9 @@
+package com.ladysparks.ttaenggrang.data.model.response
+
+data class NationInfoResponse(
+    val treasuryIncome : Int,
+    val averageStudentBalance : Double,
+    val activeItemCount : Int,
+    val classSavingsGoal : Int,
+    var isPossible: Boolean? = null // 국가 정보가 있는지 여부를 보기 위한 확인용 변수
+)
